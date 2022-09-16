17a3efe84121b2aae24616d8ad353bd30e1278b9
Rachel Samaniego Gisbert <raquel.samaniego95@gmail.com>
2022-09-16 18:48:57 +0200

    [MOD] modificated all functions - all test OK

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,45 @@
 'use strict'
 
 function letras(cadena) {
-
+	return cadena.length
 }
 
 function palabras(cadena) {
-
+	let cadenaArray = cadena.split(' ')
+	return cadenaArray.length
 }
 
 function maysc(cadena) {
-
+	return cadena.toLocaleUpperCase()
 }
 
 function titulo(cadena) {
-
+	let cadenaArray = cadena.split(' ')
+	let nuevaCadena = []
+	for(var palabra of cadenaArray) {
+		let letra = palabra.charAt(0)
+		nuevaCadena.push(palabra.replace(letra, letra.toLocaleUpperCase()))
+	}
+	return nuevaCadena.join(' ')
 }
 
 function letrasReves(cadena) {
-
+	let nuevaCadena = ''
+	for(let i = cadena.length; i >= 0; i--){
+		nuevaCadena += cadena.charAt(i)
+	}
+	return nuevaCadena
 }
 
 function palabrasReves(cadena) {
-
+	return cadena.split(' ').reverse().join(' ')
 }
 
 function palindromo(cadena) {
-
+	let cadena1 = cadena.toLowerCase().split(' ').join('')
+	let cadena2 = cadena.toLowerCase().split(' ').join('').split('').reverse().join('')
+	return (cadena1 == cadena2)? true : false
+		
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
 'use strict'
 
+do {
+    var cadena= prompt('Introduce una frase')
+} while(cadena == '')
 
+console.log(cadena)
+
+console.log(letras(cadena))
+
+console.log(palabras(cadena))
+
+console.log(maysc(cadena))
+
+console.log(titulo(cadena))
+
+console.log(letrasReves(cadena))
+
+console.log(palabrasReves(cadena))
+
+console.log(palindromo(cadena))
